5329c58df525a82ccb405285cc4c4a0cf918c64c
Sungwoo <chltjddn1313@kakao.com>
2022-07-12 04:06:13 +0900

    input작성 후 버튼클릭시 or 엔터키 입력시 li태그 추가

---
+++ main.js
@@ -0,0 +1,42 @@
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const input_btn = document.querySelector('.footer__button');
+
+input_btn.onclick = function() {
+  create_li();
+}
+
+input.addEventListener('keypress', (key)=> {
+  if (key.code == "Enter") {
+    create_li();
+  }
+});
+
+function create_li() {
+  const item_row = document.createElement('li');
+  item_row.className = 'item__row';
+  items.appendChild(item_row);
+
+  const item = document.createElement('div');
+  item.className = 'item';
+  item_row.appendChild(item);
+  
+  const item_name = document.createElement('span');
+  item_name.className = 'item__name';
+  item.appendChild(item_name);
+  
+  const item_delete = document.createElement('button');
+  item_delete.className = 'item__delete';
+  item_delete.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  item.appendChild(item_delete);
+  
+  const item_divider = document.createElement('div');
+  item_divider.className = 'item__divider';
+  item_row.appendChild(item_divider);
+
+  console.log(item_row);
+  console.log(item_name);
+  item_name.innerHTML = input.value;
+
+  input.value = null;
+}
